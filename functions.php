590592e87b73c87245439bf7734e2b337683d55b
NicolasSchwab <nicolas.schwab@ceff.ch>
2022-03-12 12:04:48 +0100

    Clean code

--- functions.php
+++ functions.php
@@ -1,11 +1,13 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'mycotra_enqueue_styles' );
-add_action( 'after_setup_theme', 'mycotra_locale' );
+add_action('wp_enqueue_scripts', 'mycotra_enqueue_styles');
+add_action('after_setup_theme', 'mycotra_locale');
 
-function mycotra_enqueue_styles(){
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+function mycotra_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 
-function mycotra_locale() {
-    load_child_theme_textdomain( 'scapeshot', get_stylesheet_directory() . '/languages' );
+function mycotra_locale()
+{
+    load_child_theme_textdomain('scapeshot', get_stylesheet_directory() . '/languages');
 }
